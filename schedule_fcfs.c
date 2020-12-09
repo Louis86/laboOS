88ad117ad56db5d15d472873dedeb71014cc8dee
louis <louishuguesa@gmail.com>
2020-12-09 23:34:48 +0100

    fcfs

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -4,63 +4,28 @@
 #include "schedulers.h"
 #include "cpu.h"
 
-int pid =0;
-void add(char *name, int priority, int burst, struct node **head)
+struct node *head;
+int numTasks = 0;
+
+void add(char *name, int priority, int burst)
 {
+	Task *tmp = malloc(sizeof(struct node));
+	tmp->name = name;
+	tmp->priority = priority;
+	tmp->burst = burst;
+	insert(&head, tmp);
+	numTasks++;
+}
 
-      Task *newtask = malloc(sizeof(Task));
-      if(newtask==NULL)
-          {
-            printf("bad \n" );
-          }
+void schedule()
+{
+	//FCFS
 
-
-      newtask->tid=pid;
-      pid++;
-      newtask->priority=priority;
-      newtask->burst=burst;
-      newtask->name=(char*)malloc(sizeof(char)*24);
-
-      if(newtask->name==NULL)
-      {
-        printf("bad \n");
-      }
-
-      strcpy(newtask->name, name);
-      insert(head, newtask);
-      //printf("[%s] [%d] [%d]\n",head->task->name, head->task->priority, head->task->burst);
-
-      traverse(*head);
-      printf("bad \n");
-      }
-
-
-void schedule(struct node *head)
-    {
-      int time=0;
-      int i;
-      int j=0;
-      struct node *temp=NULL;
-      i=poplast(&head, &temp);
-      while(i==0)
-      {
-        printf("ok");
-        run(temp->task,1);
-        time+=temp->task->burst;
-        free(temp->task->name);
-        free(temp->task);
-        free(temp);
-        i=poplast(&head, &temp);
-        j++;
-      }
-      run(head->task, 1);
-      float waiting= (float)time/(float)(j+1);
-      time+=head->task->burst;
-      free(head->task->name);
-      free(head->task);
-      free(head);
-      float turn= (float)time/(float)(j+1);
-      printf("%f Average waiting time \n", waiting);
-      printf("%f Average turnaround time \n", turn);
-
-    }
+	while(numTasks > 0)
+	{
+		Task *tmp = head->task;
+		run(tmp, tmp->burst);
+		delete(&head, tmp);
+		numTasks--;
+	}
+}
